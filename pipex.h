526cdb2854e356a0aee1742a903d146fd9da8620
Wassim Daoudi Idrissi <wdaoudi-@made-f0Ar11s7.clusters.42paris.fr>
2024-10-14 15:21:11 +0200

    modif du point h

--- pipex.h
+++ pipex.h
@@ -1,17 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   pipex.h                                            :+:      :+:    :+:   */
+/*   pipex_bonus.h                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: wdaoudi- <wdaoudi-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/30 13:57:51 by wdaoudi-          #+#    #+#             */
-/*   Updated: 2024/10/14 13:51:24 by wdaoudi-         ###   ########.fr       */
+/*   Updated: 2024/10/14 14:01:03 by wdaoudi-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PIPEX_H
-# define PIPEX_H
+#ifndef PIPEX_BONUS_H
+# define PIPEX_BONUS_H
 
 # include "./libft/libft.h"
 # include "./printf/ft_printf.h"
@@ -32,9 +32,14 @@ typedef struct s_data
 	char	**path;
 	int		ac;
 	int		cmd_count;
+	int		cmd_start;
 	char	**cmd;
 	pid_t	*pid;
 	int		prev_fd;
+	int		is_here_doc;
+	int		decorless;
+	char	*limiter;
+	int		here_doc_fd;
 }			t_data;
 
 // pathing
@@ -47,6 +52,7 @@ char		**add_slash_to_paths(char **paths);
 // parsing + check
 
 void		init_data(t_data *data, int ac, char **av, char **env);
+void		init_data2(t_data *data, bool is_here_doc);
 void		opening_file(t_data *data);
 char		*have_access(t_data *data, char *cmd);
 char		**parsing_cmd(t_data *data, int i);
@@ -54,15 +60,16 @@ void		path_not_found(char **cmd);
 
 // exec
 int			looking_using(t_data *data, char **cmd, int i);
-void		redirect(t_data *data, int i, char **cmd, int fd[]);
-void		ft_exec_first(t_data *data, char **cmd, int fd[]);
+void		sub_looking(int *fd, t_data *data);
+void		redirect(t_data *data, int i, char **cmd, int *fd);
+void		ft_exec_first(t_data *data, char **cmd, int *fd);
 void		ft_exec_last(t_data *data, char **cmd);
-void		ft_exec(t_data *data, char **cmd, int fd[]);
+void		ft_exec(t_data *data, char **cmd, int *fd);
 
 // errror
 
 void		error_opening(t_data *data);
-int			ft_fd_first(int fd_first, t_data *data);
+int			ft_fd_first(t_data *data);
 void		ft_impossible(t_data *data);
 
 // utils
@@ -72,5 +79,21 @@ void		free_array(char **arr);
 void		free_end(t_data *data);
 int			waiting(t_data *data);
 void		ft_close(int *fd1, int *fd2, int *fd3, t_data *data);
+
+// reduction de focntion
+
+int			check_arguments(int ac, char **av, t_data *data);
+int			setup_here_doc(t_data *data);
+int			execute_commands(t_data *data);
+void		cleanup_and_wait(t_data *data, int *status);
+void		sub_exec(t_data *data, char **cmd);
+void		sub_init(t_data *data, char **av, char **env, int ac);
+
+// here_doc
+
+int			ft_is_here_doc(char **av, t_data *data);
+int			handle_here_doc(t_data *data);
+void		process_here_doc_input(t_data *data, int pipe_fd[2]);
+char		*here_doc_on_first_exec(t_data *data, char **cmd, int *fd);
 
 #endif
